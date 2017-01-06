6673a3ab9ef197f052b0f6d2bdacc04bc5560fa4
Heidi Goosen <Heidi Goosen>
2017-01-06 16:42:21 +1300

    alerts working

--- client/dateHelpers.js
+++ client/dateHelpers.js
@@ -23,62 +23,9 @@ function currentTime(){
 }
 
 
-function countdownTimer(){
-  var uctTime = listen.momentUctLong
-  console.log("this is momentUctLongTime", uctTime)
-  var currentGtm = listen.gtmTime
-  console.log("this is gtmTimeValue", currentGtm)
-  setInterval(function(){
-    var hours = moment.utc(moment(uctTime,"DD-MM-YYYY HH:mm:ss").diff(moment(currentGtm,"DD-MM-YYYY HH:mm:ss"))).hours()
-    var minutes = moment.utc(moment(uctTime,"DD-MM-YYYY HH:mm:ss").diff(moment(currentGtm,"DD-MM-YYYY HH:mm:ss"))).minutes()
-    var seconds = moment.utc(moment(uctTime,"DD-MM-YYYY HH:mm:ss").diff(moment(currentGtm,"DD-MM-YYYY HH:mm:ss"))).seconds()
-
-    $('#hours').text(hours)
-    $('#minutes').text(minutes)
-    $('#seconds').text(seconds)
-  }, 1000
-  )
-  return setInterval
-}
-
-/*
-
-function countdownTimer(timeGtm){
-  setInterval(function(){
-    var mctTime = 0300
-    var hours = moment.duration(mctTime - moment().tz('Europe/London')).hours()
-    var minutes = moment.duration(mctTime - moment().tz('Europe/London')).minutes()
-    var seconds = moment.duration(mctTime - moment()).seconds()
-
-    $('#hours').text(hours)
-    $('#minutes').text(minutes)
-    $('#seconds').text(seconds)
-  }, 1000
-  )
-  return setInterval
-}
-*/
-
-/*
-function countdownTimer(dateOfBirth){
-  setInterval(function(){
-    var deathDate = moment(dateOfBirth).add(81.251, 'years')
-    var minutes = moment.duration(deathDate - moment()).minutes()
-    var seconds = moment.duration(deathDate - moment()).seconds()
-    $('#years').text(years)
-    $('#months').text(months)
-    $('#days').text(months)
-    $('#minutes').text(minutes)
-    $('#seconds').text(seconds)
-  }, 1000
-  )
-  return setInterval
-}
-*/
 
 module.exports = {
   today:today,
   currentTime:currentTime,
   timeGtm:timeGtm,
-  countdownTimer:countdownTimer,
 }
--- client/listener.js
+++ client/listener.js
@@ -3,6 +3,8 @@ var $ = require('jquery')
 //var controller = require('./controller')
 var dateHelpers = require('./dateHelpers')
 var moment = require('moment');
+var momentCountdown = require('moment-countdown')
+var momentZone = require('moment-timezone')
 
 var todaysDate = dateHelpers.today()
 var todaysTime = dateHelpers.currentTime()
@@ -10,16 +12,15 @@ var gtmTime = dateHelpers.timeGtm()
 console.log("this is gtmTime", gtmTime)
 var time = ""
 var epoch = ""
+var gtmTimeEpoch = ""
 var momentUct = ""
 var momentUctLong = ""
 var difference = ""
 
 function listen(){
 
-  var gtmTimeShort = moment(gtmTime).tz('Europe/London').format("HH:mm:ss")
-  console.log("this is gtmTimeShort", gtmTimeShort)
+  var gtmTimeShort = moment(gtmTime).tz('Europe/London').format("HH:mm")
   var gtmDateShort = moment(gtmTime).tz('Europe/London').format("D MMM")
-  console.log("this is gtmDateShort", gtmDateShort)
 
     $("#todays-date").append(todaysDate)
     $("#todays-time").append(todaysTime)
@@ -28,14 +29,13 @@ function listen(){
 
     $('#time-submit').click(function(e){
     e.preventDefault()
+          gtmTimeEpoch = new Date(gtmTime).valueOf()
           time = $('#mct-input').val()
           epoch = new Date(time).valueOf()
           momentUct = moment.utc(epoch).format("HH:mm:ss")
           momentUctLong = moment.utc(epoch).format()
-          console.log("this is momentUctLong", momentUctLong)
           $('#mct-time').html(momentUct)
-
-          difference = moment.utc(moment(momentUctLong,"DD-MM-YYYY HH:mm:ss").diff(moment(gtmTime,"DD-MM-YYYY HH:mm:ss"))).format("HH:mm:ss")
+          difference = moment(moment(momentUctLong,"DD-MM-YYYY HH:mm:ss").diff(moment(gtmTime,"DD-MM-YYYY HH:mm:ss"))).format("HH:mm:ss")
           console.log("this is difference", difference)
 
           countdownTimer()
@@ -43,9 +43,38 @@ function listen(){
         });
 }
 
+var intervalID = setInterval(countdownTimer, 1000)
+
 function countdownTimer(){
+    intervalHours =moment.duration(epoch - moment().tz('Europe/London')).hours()
+    intervalMinutes = moment.duration(epoch - moment().tz('Europe/London')).minutes()
+    intervalSeconds = moment.duration(epoch - moment().tz('Europe/London')).seconds()
+    $('#hours').text(intervalHours)
+    $('#minutes').text(intervalMinutes)
+    $('#seconds').text(intervalSeconds)
+
+  alertNotifiction()
+  return setInterval
+}
+
+
+function alertNotifiction(){
+  if (intervalHours == 0 && intervalMinutes == 30){
+     alert("30 minutes to go.. tick tock")
+  } else if(intervalHours == 0 && intervalMinutes == 15){
+      alert("15 minutes to go.. tick tock")
+  } else if (intervalHours == 0 && intervalMinutes == 5) {
+      alert("5 minutes to go.. tick tock")
+  } else if (intervalHours == 0 && intervalMinutes == 0 && intervalSeconds == 0)
+        alert("times up")
+        clearInterval(intervalID)
+}
+
+
+/*function countdownTimer(){
     var countdownTimer = dateHelpers.countdownTimer(gtmTime)
   }
+  */
 
 
 module.exports = {
