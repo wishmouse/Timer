8c3d1c7c69a91df65987ed9539494de78280dfae
Heidi Goosen <Heidi Goosen>
2017-01-08 18:57:09 +1300

    all countdowns working

--- client/dateHelpers.js
+++ client/dateHelpers.js
@@ -8,25 +8,58 @@ var listen = require('./listener')
 moment() .format();
 
 
-function today(){
-  return moment().format('D MMM')
+function currentDate(){
+  console.log("this is today")
+  setInterval(function(){
+    var day = moment().format("DD")
+    var month = moment().format("MMM")
+    var second = moment().format("ss")
+
+    $("#todays-date").text(day +" "+ month)
+  }, 1000
+  )
+  return setInterval
+}
+
+function currentTime(){
+  setInterval(function(){
+    var hours = moment().hours()
+    var minutes = moment().minutes()
+    var seconds = moment().seconds()
+
+    $("#todays-time").text(hours +':'+ minutes +':'+ seconds)
+  }, 1000
+  )
+  return setInterval
 }
 
 function timeGtm(){
   var localTime = moment()
-  return localTime.tz('Europe/London').format()
+  localTime.utc().format()
+  setInterval(function(){
+    var hours = moment.utc().hours()
+    var minutes = moment.utc().minutes()
+    var seconds = moment.utc().seconds()
+
+    $("#todays-time-gtm").text(hours +':'+ minutes +':'+ seconds)
+  }, 1000
+  )
+  return setInterval
 }
 
+function dateGtm(){
+  setInterval(function(){
+    var day = moment.utc().format("DD")
+    var month = moment.utc().format("MMM")
+    var second = moment.utc().format("ss")
 
-function currentTime(){
-  return moment().format('HH:mm')
+    $("#todays-date-gtm").text(day +" "+ month)
+  }, 1000
+  )
+  return setInterval
 }
 
 function countdownTimer(){
-  var uctTime = listen.momentUctLong
-   console.log("this is momentUctLongTime", uctTime)
-   var currentGtm = listen.gtmTime
-   console.log("this is gtmTimeValue", currentGtm)
    setInterval(function(){
      var hours = moment.utc(moment(uctTime,"DD-MM-YYYY HH:mm:ss").diff(moment(currentGtm,"DD-MM-YYYY HH:mm:ss"))).hours()
      var minutes = moment.utc(moment(uctTime,"DD-MM-YYYY HH:mm:ss").diff(moment(currentGtm,"DD-MM-YYYY HH:mm:ss"))).minutes()
@@ -41,8 +74,9 @@ function countdownTimer(){
  }
 
 module.exports = {
-  today:today,
+  currentDate:currentDate,
   currentTime:currentTime,
   timeGtm:timeGtm,
+  dateGtm:dateGtm, 
   countdownTimer:countdownTimer
 }
--- client/listener.js
+++ client/listener.js
@@ -6,45 +6,90 @@ var moment = require('moment');
 var momentCountdown = require('moment-countdown')
 var momentZone = require('moment-timezone')
 
-var todaysDate = dateHelpers.today()
+var timeZone = ""
+var todaysDate = dateHelpers.currentDate()
 var todaysTime = dateHelpers.currentTime()
 var gtmTime = dateHelpers.timeGtm()
-console.log("this is gtmTime", gtmTime)
+var gtmDate = dateHelpers.dateGtm()
+var gtmTimeShort = ""
+var gtmDateShort = ""
 var time = ""
+var ectTime = ""
 var epoch = ""
 var gtmTimeEpoch = ""
 var momentUct = ""
+var momentEct = ""
 var momentUctLong = ""
+var momentEctLong =""
 var difference = ""
 
+
+
 function listen(){
-
-  var gtmTimeShort = moment(gtmTime).tz('Europe/London').format("HH:mm")
-  var gtmDateShort = moment(gtmTime).tz('Europe/London').format("D MMM")
-
-    $("#todays-date").append(todaysDate)
-    $("#todays-time").append(todaysTime)
-    $("#todays-time-gtm").append(gtmTimeShort)
-    $("#todays-date-gtm").append(gtmDateShort)
 
     $('#time-submit').click(function(e){
     e.preventDefault()
           gtmTimeEpoch = new Date(gtmTime).valueOf()
           time = $('#mct-input').val()
           epoch = new Date(time).valueOf()
-          momentUct = moment.utc(epoch).format("HH:mm:ss")
-          momentUctLong = moment.utc(epoch).format()
-          $('#mct-time').html(momentUct)
+          mctTimeShort = moment.utc(epoch).format("HH:mm")
+          mctDateShort = moment.utc(epoch).format("D MMM")
+          $('#mct-time').html(mctTimeShort)
           difference = moment(moment(momentUctLong,"DD-MM-YYYY HH:mm:ss").diff(moment(gtmTime,"DD-MM-YYYY HH:mm:ss"))).format("HH:mm:ss")
-          console.log("this is difference", difference)
 
-          countdownTimer()
+          console.log("this is todays date", gtmDateShort)
+          console.log("this is mctDateShort", mctDateShort)
+          console.log("this is gtmTimeShort", gtmTimeShort)
+          console.log("this is mctTimeShort", mctTimeShort)
+
+          timeType()
+          countdownTimerMct()
         });
+
+      $('#time-submit-ect').click(function(e){
+      e.preventDefault()
+            gtmTimeEpoch = new Date(gtmTime).valueOf()
+            ectTime = $('#ect-input').val()
+            epoch = new Date(ectTime).valueOf()
+            ectTimeShort = moment.utc(epoch).format("HH:mm")
+            ectDateShort = moment.utc(epoch).format("D MMM")
+            $('#ect-time').html(ectTimeShort)
+
+            timeType()
+            countdownTimerEct()
+          });
 }
 
-var intervalID = setInterval(countdownTimer, 1000)
 
-function countdownTimer(){
+function timeType(){
+    if(gtmDateShort == mctDateShort && gtmTimeShort < mctTimeShort ){
+      timeZone = "MCT"
+      console.log("this is time Zone", timeZone)
+      //alert("time zone = mct")
+    } else if (gtmDateShort == ectDateShort && gtmTimeShort < ectTimeShort && mctTimeShort < ectTimeShort){
+      timeZone = "ECT"
+      console.log("this is time Zone", timeZone)
+      //alert("time zone = ect")
+    }
+}
+
+
+var intervalMct = setInterval(countdownTimerMct, 1000)
+var intervalEct = setInterval(countdownTimerEct, 1000)
+
+function countdownTimerMct(){
+    intervalHours =moment.duration(epoch - moment().tz('Europe/London')).hours()
+    intervalMinutes = moment.duration(epoch - moment().tz('Europe/London')).minutes()
+    intervalSeconds = moment.duration(epoch - moment().tz('Europe/London')).seconds()
+    $('#hours').text(intervalHours)
+    $('#minutes').text(intervalMinutes)
+    $('#seconds').text(intervalSeconds)
+
+  alertNotifiction()
+  return setInterval
+}
+
+function countdownTimerEct(){
     intervalHours =moment.duration(epoch - moment().tz('Europe/London')).hours()
     intervalMinutes = moment.duration(epoch - moment().tz('Europe/London')).minutes()
     intervalSeconds = moment.duration(epoch - moment().tz('Europe/London')).seconds()
@@ -60,35 +105,44 @@ function countdownTimer(){
 function alertNotifiction(){
   if (intervalHours == 0 && intervalMinutes == 30 && intervalSeconds < 59 && intervalSeconds > 50) {
      thirtyMinutes()
+     $("#time-input").hide()
     document.body.appendChild(soundThirty).play()
   } else if (intervalHours == 0 && intervalMinutes == 30 && intervalSeconds == 44){
       $( "audio" ).remove( "#audio" )
+      $("#time-input").show()
   } else if(intervalHours == 0 && intervalMinutes == 15 && intervalSeconds < 59 && intervalSeconds > 45) {
      fifteenMinutes()
+     $("#time-input").hide()
       document.body.appendChild(soundFifteen).play()
   } else if (intervalHours == 0 && intervalMinutes == 15 && intervalSeconds == 44) {
       $( "audio" ).remove( "#audio" )
+      $("#time-input").show()
   } else if (intervalHours == 0 && intervalMinutes == 5  && intervalSeconds < 59 && intervalSeconds > 45) {
+      $("#time-input").hide()
      fiveMinutes()
      document.body.appendChild(soundFive).play()
   } else if (intervalHours == 0 && intervalMinutes == 5 && intervalSeconds == 44 ) {
       $( "audio" ).remove( "#audio" )
+      $("#time-input").show()
   } else if (intervalHours == 0 && intervalMinutes == 0 && intervalSeconds < 59 && intervalSeconds > 45) {
     zeroMinutes()
+    $("#time-input").hide()
     document.body.appendChild(soundEnd).play()
   }  else if (intervalHours == 0 && intervalMinutes == 0 && intervalSeconds == 44) {
-      $( "audio" ).remove( "#audio" )
-  } else if (intervalHours == 0 && intervalMinutes == 0 && intervalSeconds < 10 && intervalSeconds > 1) {
-  console.log("do nothing")
+      $("audio" ).remove( "#audio" )
+      $("#time-input").show()
+  } else if (intervalHours == 0 && intervalMinutes == 0 && intervalSeconds == 0 && timeZone == "MCT") {
+
   }
 }
+
 
 
 $('div.section:empty').hide();
 
   function thirtyMinutes(){
     document.getElementById("timer-message").innerHTML = "30 minutes ....";
-    $('#timer-message').fadeOut(500).fadeIn(500);
+    $('#timer-message').fadeOut(500).fadeIn(500)
     setTimeout ( 'document.getElementById("timer-message").innerHTML = ""', 500 );
   }
 
@@ -106,7 +160,7 @@ $('div.section:empty').hide();
 
   function zeroMinutes(){
     document.getElementById("timer-message").innerHTML = "times up ....";
-     $('#timer-message').fadeOut(500).fadeIn(500);
+     $('#timer-message').fadeOut(500).fadeIn(500).addClass("timer-message");
      setTimeout ( 'document.getElementById("timer-message").innerHTML = ""', 500 )
    }
 
